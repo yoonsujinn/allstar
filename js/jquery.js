9be1bb13248f652a5d97a5e5f9209e1bf6ce852a
yoonsujinn <yoonsujin2@naver.com>
2023-09-07 11:11:56 +0900

    g
    gg

--- js/jquery.js
+++ js/jquery.js
@@ -18,34 +18,53 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // page_4_li active_1 on off
     document.addEventListener("DOMContentLoaded", function() {
-        const page_4_list = document.querySelectorAll('.page_4_li');
+        const page_4_li = document.querySelectorAll('.page_4_li');
 
+        const page_4_li_1 = document.querySelectorAll('#page_4_li_1');
+        const page_4_li_2 = document.querySelectorAll('#page_4_li_2');
+        const page_4_li_3 = document.querySelectorAll('#page_4_li_3');
 
-        page_4_list.forEach((liElement) => {
+        page_4_li.forEach((liElement) => {
             liElement.addEventListener("click", function(e) {
-              page_4_list.forEach((otherLi) => {
+              page_4_li.forEach((otherLi) => {
              otherLi.classList.remove('active_1');
          });
          // 클릭한 li 요소에 'active_1' 클래스를 추가합니다.
          liElement.classList.add('active_1');
+          if (document.querySelector('#page_4_li_1').classList.contains('active_1')) {
+              document.querySelector('#rec_4_right_1').classList.add('active3')
+              document.querySelector('#rec_4_right_2').classList.remove('active3');
+              document.querySelector('#rec_4_right_3').classList.remove('active3');
+        } else if (document.querySelector('#page_4_li_2').classList.contains('active_1')) {
+            document.querySelector('#rec_4_right_2').classList.add('active3')
+            document.querySelector('#rec_4_right_1').classList.remove('active3');
+            document.querySelector('#rec_4_right_3').classList.remove('active3');
+        } else if (document.querySelector('#page_4_li_3').classList.contains('active_1')) {
+          document.querySelector('#rec_4_right_3').classList.add('active3')
+          document.querySelector('#rec_4_right_2').classList.remove('active3');
+          document.querySelector('#rec_4_right_1').classList.remove('active3');
+        }
             });
           });
         });
-    document.addEventListener("DOMContentLoaded", function() {
-    const pElements = document.querySelectorAll('#rec_4 p');
 
-    pElements.forEach((pElement) => {
-        pElement.addEventListener("click", function(e) {
-            // 모든 <p> 요소에서 'active_3' 클래스를 제거합니다.
-            pElements.forEach((otherP) => {
-                otherP.classList.remove('active_3');
+
+        // page_4_web_list_1 active_1 on off
+        document.addEventListener("DOMContentLoaded", function() {
+            const page_4_web_list_1 = document.querySelectorAll('.page_4_web_list_1');
+            page_4_web_list_1.forEach((liElement) => {
+                liElement.addEventListener("click", function(e) {
+                  page_4_web_list_1.forEach((otherLi) => {
+                 otherLi.classList.remove('active_2');
+             });
+             // 클릭한 li 요소에 'active_1' 클래스를 추가합니다.
+             liElement.classList.add('active_2');
+                });
+              });
             });
 
-            // 클릭한 <p> 요소에 'active_3' 클래스를 추가합니다.
-            pElement.classList.add('active_3');
-        });
-    });
-});
+
+
 
 
 
@@ -91,15 +110,15 @@ $(document).ready(function() {
           if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
               // 마우스 휠을 위로 스크롤했을 때
               newScrollTop = currentScrollTop - elementHeight;
-              console.log(newScrollTop+"실제 이동되는 스크롤");
-              console.log(currentScrollTop+"현재스크롤위치");
-              console.log(elementHeight+"화면높이");
+              // console.log(newScrollTop+"실제 이동되는 스크롤");
+              // console.log(currentScrollTop+"현재스크롤위치");
+              // console.log(elementHeight+"화면높이");
           } else {
               // 마우스 휠을 아래로 스크롤했을 때
               newScrollTop = currentScrollTop + elementHeight;
-              console.log(newScrollTop+"실제 이동되는 스크롤");
-              console.log(currentScrollTop+"현재스크롤위치");
-              console.log(elementHeight+"화면높이");
+              // console.log(newScrollTop+"실제 이동되는 스크롤");
+              // console.log(currentScrollTop+"현재스크롤위치");
+              // console.log(elementHeight+"화면높이");
           }
 
           // 스크롤 애니메이션 적용
